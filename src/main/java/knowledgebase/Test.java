913a171821a778957dfa12cce1df8141e4d9b6ba
ligenqiang <ligenqiang8@126.com>
2026-03-18 11:39:27 +0800

    更改test456文件

--- src/main/java/knowledgebase/Test.java
+++ src/main/java/knowledgebase/Test.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws IOException {
         EmbeddingGenerator.generateEmbedding("SQL优化指南");
-        System.out.println(123);
+        System.out.println(123456);
     }
 }
